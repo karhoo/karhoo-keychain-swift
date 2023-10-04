bc5b50de87f222ffa6e6a488a9becf8d1e7abd5e
dianakarh <86302636+dianakarh@users.noreply.github.com>
2023-10-04 15:13:21 +0300

    MOB-4887 update deployment target (#1)
    * gitignore
    
    * add files with bumped deployment target

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.7.0
+import PackageDescription
+
+let package = Package(
+    name: "KeychainSwift",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(name: "KeychainSwift", targets: ["KeychainSwift"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+          name: "KeychainSwift",
+          dependencies: [],
+          path: "Sources",
+          exclude: ["Info.plist"]
+        ),
+        .testTarget(
+            name: "KeychainSwiftTests",
+            dependencies: ["KeychainSwift"],
+            exclude: ["ClearTests.swift"]
+        )
+    ]
+)
